98401b74ecc112c1c8331a4efb2ac2da1318f4c4
CallumGoodley <callumgoodley@btinternet.com>
2020-02-03 17:14:26 +0000

    app displaying articles that can be shown by topic and sorted by the specified requirements

--- nc-news-app/src/Components/Article.jsx
+++ nc-news-app/src/Components/Article.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ArticlesList from './ArticlesList';
 
 const Article = (props) => {
+	console.log(props);
 	return <ArticlesList article_id={props.article_id} />;
 };
 
