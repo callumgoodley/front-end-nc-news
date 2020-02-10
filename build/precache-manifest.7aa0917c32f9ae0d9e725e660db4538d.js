da705d63ed2e942eace3eddc41e25c6cd3f6f3a6
CallumGoodley <callumgoodley@btinternet.com>
2020-02-10 11:21:31 +0000

    layer of nesting removed

---
+++ build/precache-manifest.7aa0917c32f9ae0d9e725e660db4538d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459de0e91fa87f7504d385ebf98de86b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "321b7326504971bd9ee3",
+    "url": "/static/css/main.84827521.chunk.css"
+  },
+  {
+    "revision": "2b9c0c91e5b9070358bf",
+    "url": "/static/js/2.2cd0e9c6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2cd0e9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "321b7326504971bd9ee3",
+    "url": "/static/js/main.b909e700.chunk.js"
+  },
+  {
+    "revision": "bf2dbb813ab13f8157dd",
+    "url": "/static/js/runtime-main.d3e2165c.js"
+  }
+]);
